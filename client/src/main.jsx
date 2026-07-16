37c501a7c8ba4f77ef1cc8449bdd59c03704cd8d
Prajakta Bramhe <prajaktabramhe@gmail.com>
2026-07-16 12:28:41 +0530

    Added some minor changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
 import App from "./App";
 import "./index.css";
-
 import AuthProvider from "./context/AuthContext";
-
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
